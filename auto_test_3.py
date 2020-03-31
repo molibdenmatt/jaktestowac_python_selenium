fa81e7e6151726f5ee480de6387a3cadd1201384
Mateusz Swieton <mateusz.swieton@gmail.com>
2020-03-31 21:17:08 +0200

    pt1-mk3-s08-15

--- auto_test_3.py
+++ auto_test_3.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class MainTests(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -21,3 +22,34 @@ class MainTests(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
+
+
+class LoginPageTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+    def test_page_title(self):
+        driver = self.driver
+        url = "https://demobank.jaktestowac.pl/logowanie_etap_1.html"
+        driver.get(url)
+        page_h1_title = driver.find_element_by_xpath("//div[contains(@class, 'login-container')]//h1").text
+        self.assertEqual('Wersja demonstracyjna serwisu demobank', page_h1_title,
+                         f'Expected title differ from actual title for page url: {url}')
+
+    def test_is_login_disabled(self):
+        driver = self.driver
+        url = "https://demobank.jaktestowac.pl/logowanie_etap_1.html"
+        driver.get(url)
+        identity_input = driver.find_element_by_id('login_id')
+        identity_input.clear()
+        login_next_button = driver.find_element_by_id('login_next')
+        login_next_button_disabled = login_next_button.get_property('disabled')
+        self.assertEqual(True, login_next_button_disabled,
+                         f'Expected state of "dalej" button: True , differ from actual: '
+                         f'{login_next_button_disabled} , for page url: {url}')
